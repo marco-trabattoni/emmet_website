e304f2f67d9b690e912f6d3652e99478bd37801e
Marco Trabattoni <95588163+marco-trabattoni@users.noreply.github.com>
2026-03-27 01:01:27 +0100

    invertito 2 step form

--- script.js
+++ script.js
@@ -1,135 +1,157 @@
 function scrollEmailIntoView() {
-    const input = document.getElementById('email-input');
     setTimeout(() => {
         window.scrollTo({
             top: 300,
-            behavior: 'smooth'
+            behavior: "smooth"
         });
-    }, 600); // attesa per evitare conflitto con apertura tastiera
+    }, 600);
 }
-
-
-const buttons = document.querySelectorAll('.bottons');
-const tagliaInput = document.getElementById('taglia-input');
-const button = document.getElementById('button');
-
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-
-        buttons.forEach(b => b.classList.remove('active'));
-
-        btn.classList.add('active');
-        button.style.opacity = "1";
-        button.style.pointerEvents = "auto";
-
-        tagliaInput.value = btn.getAttribute('data-size');
-    });
-});
-
-
-let inseritoMail = false;
-
-const emailInput = document.getElementById('email-input');
-const targetDiv = document.getElementById('next');
-
-emailInput.addEventListener('blur', () => {
-
-    if (emailInput.value.trim() !== '') {
-        targetDiv.style.opacity = '1'; // mostra il div
-        targetDiv.style.pointerEvents = 'auto';
-        inseritoMail = true;
-    } else {
-        targetDiv.style.opacity = '0.3'; // nasconde il div
-        targetDiv.style.pointerEvents = 'auto';
-        inseritoMail = false;
-    }
-});
-
-
-document.getElementById('form').addEventListener('submit', function (e) {
-    if (!tagliaInput.value) {
-        alert('Per favore, seleziona una taglia.');
-        e.preventDefault();
-    }
-});
-
 
 const form = document.getElementById("form");
 const testi = document.getElementById("testi");
+const cta = document.getElementById("cta");
+
+const firstStep = document.getElementById("first-step");
+const secondStepBox = document.getElementById("second-step");
+
+const emailInput = document.getElementById("email-input");
+const nextButton = document.getElementById("next");
+
+const sizeButtons = document.querySelectorAll(".bottons");
+const sizeInput = document.getElementById("taglia-input");
+const submitButton = document.getElementById("button");
+
+const messaggioConferma = document.getElementById("messaggio-conferma");
+
+let emailValida = false;
+let currentStep = 1;
+
+/* stato iniziale */
+submitButton.disabled = true;
+submitButton.style.opacity = "0.3";
+submitButton.style.pointerEvents = "none";
+
+function aggiornaStatoNext() {
+
+        const tagliaSelezionata = sizeInput.value.trim() !== "";
+    console.log(tagliaSelezionata);
+
+    if (tagliaSelezionata) {
+        nextButton.style.opacity = "1";
+        nextButton.style.pointerEvents = "auto";
+    } else {
+        nextButton.style.opacity = "0.3";
+        nextButton.style.pointerEvents = "none";
+    }
+
+
+}
+
+function aggiornaStatoSubmit() {
+
+    emailValida = emailInput.value.trim() !== "" && emailInput.checkValidity();
+
+    if (emailValida && currentStep === 2) {
+        submitButton.disabled = false;
+        submitButton.style.opacity = "1";
+        submitButton.style.pointerEvents = "auto";
+    } else {
+        submitButton.disabled = true;
+        submitButton.style.opacity = "0.3";
+        submitButton.style.pointerEvents = "none";
+    }
+
+}
 
 function cambiaStile() {
-
     form.style.pointerEvents = "auto";
     form.style.opacity = "1";
 
     testi.style.opacity = "0";
     testi.style.pointerEvents = "none";
 
-    input.style.pointerEvents = "auto";
-    uno.style.opacity = "1";
-
-    if (inseritoMail == true) {
-        next.style.opacity = "1";
-        next.style.pointerEvents = "auto";
-    } else {
-        next.style.opacity = "0.3";
-        next.style.pointerEvents = "none";
-    }
+    aggiornaStatoNext();
 }
 
-document.getElementById("cta").addEventListener("click", cambiaStile);
 
 
-const uno = document.getElementById("first-step");
-const input = document.getElementById("email-input");
-const due = document.getElementById("second-step");
-const next = document.getElementById("next");
+
+cta.addEventListener("click", cambiaStile);
+
+emailInput.addEventListener("input", aggiornaStatoSubmit);
+emailInput.addEventListener("blur", aggiornaStatoSubmit);
+
+/* se da mobile premono il tasto blu della tastiera, NON deve submitare il form */
+emailInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+
+        if (emailInput.checkValidity() && emailInput.value.trim() !== "") {
+            submitta();
+        } else {
+            emailInput.reportValidity();
+        }
+    }
+});
+
+sizeButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        sizeButtons.forEach((b) => b.classList.remove("active"));
+        btn.classList.add("active");
+
+        sizeInput.value = btn.getAttribute("data-size");
+        aggiornaStatoNext();
+    });
+});
+
 
 
 function goBack() {
+currentStep = 1;
 
-    next.style.opacity = "1";
-    next.style.pointerEvents = "auto";
-    uno.style.opacity = "1";
-    input.style.pointerEvents = "auto";
+    nextButton.style.opacity = "1";
+    nextButton.style.pointerEvents = "auto";
 
-    due.style.opacity = "0";
-    due.style.pointerEvents = "none";
+    firstStep.style.opacity = "1";
+    emailInput.style.pointerEvents = "auto";
+
+    secondStepBox.style.opacity = "0";
+    secondStepBox.style.pointerEvents = "none";
 
     form.classList.remove("up");
 
     setTimeout(() => {
-        uno.onclick = null;
+        firstStep.onclick = null;
     }, 100);
-
-
 }
 
-
-
 function secondStep() {
+    /*
+    if (!emailInput.checkValidity() || emailInput.value.trim() === "") {
+        emailInput.reportValidity();
+        return;
+    }*/
 
-    next.style.opacity = "0";
-    next.style.pointerEvents = "none";
+    currentStep = 2;
 
-    uno.style.opacity = "0.5";
-    input.style.pointerEvents = "none";
+    nextButton.style.opacity = "0";
+    nextButton.style.pointerEvents = "none";
 
-    due.style.opacity = "1";
-    due.style.pointerEvents = "auto";
+    firstStep.style.opacity = "0.5";
+    
+    emailInput.style.pointerEvents = "auto";
+    secondStepBox.style.opacity = "1";
+    secondStepBox.style.pointerEvents = "auto";
 
     form.classList.add("up");
 
     setTimeout(() => {
-        uno.onclick = goBack;
+        firstStep.onclick = goBack;
     }, 100);
-
 }
 
-
-
-
 function esci() {
+currentStep = 1;
 
     form.style.pointerEvents = "none";
     form.style.opacity = "0";
@@ -137,50 +159,69 @@ function esci() {
     testi.style.opacity = "1";
     testi.style.pointerEvents = "auto";
 
-    next.style.pointerEvents = "none";
+    nextButton.style.pointerEvents = "none";
+    nextButton.style.opacity = "0.3";
 
-    input.style.pointerEvents = "auto";
+    emailInput.style.pointerEvents = "auto";
+    firstStep.style.opacity = "1";
 
-    due.style.opacity = "0";
-    due.style.pointerEvents = "none";
+    secondStepBox.style.opacity = "0";
+    secondStepBox.style.pointerEvents = "none";
 
     form.classList.remove("up");
 
     setTimeout(() => {
-        uno.onclick = null;
+        firstStep.onclick = null;
     }, 100);
 }
 
 
-const messaggio = document.getElementById('messaggio-conferma');
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault(); // Impedisce il reindirizzamento
+form.addEventListener("submit", submitta);
+
+async function submitta(e) {
+    e.preventDefault();
+
+    if (!emailInput.checkValidity() || emailInput.value.trim() === "") {
+        emailInput.reportValidity();
+        return;
+    }
+
+    /*
+    if (!sizeInput.value.trim()) {
+        alert("Per favore, seleziona una taglia.");
+        return;
+    }
+    */
 
     const formData = new FormData(form);
 
-    const response = await fetch("https://formspree.io/f/mrbqnvlo", {
-        method: "POST",
-        body: formData,
-        headers: {
-            'Accept': 'application/json'
+    try {
+        const response = await fetch("https://formspree.io/f/mrbqnvlo", {
+            method: "POST",
+            body: formData,
+            headers: {
+                Accept: "application/json"
+            }
+        });
+
+        if (response.ok) {
+            form.style.display = "none";
+            messaggioConferma.style.display = "block";
+
+            let metaThemeColor = document.querySelector('meta[name="theme-color"]');
+
+            if (!metaThemeColor) {
+                metaThemeColor = document.createElement("meta");
+                metaThemeColor.setAttribute("name", "theme-color");
+                document.head.appendChild(metaThemeColor);
+            }
+
+            metaThemeColor.setAttribute("content", "yellow");
+        } else {
+            alert("Si è verificato un errore. Riprova.");
         }
-    });
-
-    if (response.ok) {
-        form.style.display = 'none';
-        messaggio.style.display = 'block';
-        let metaThemeColor = document.querySelector("meta[name=theme-color]");
-
-        if (!metaThemeColor) {
-            metaThemeColor = document.createElement('meta');
-            metaThemeColor.setAttribute('name', 'theme-color');
-            document.head.appendChild(metaThemeColor);
-        }
-
-        metaThemeColor.setAttribute("content", "yellow");
-    } else {
+    } catch (error) {
         alert("Si è verificato un errore. Riprova.");
     }
-});
-
+}
